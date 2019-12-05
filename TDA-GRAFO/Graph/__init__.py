6975b5c09bd5bce826ae1f4b331654ed106b0511
thiagokovnat <tkovnat@fi.uba.ar>
2019-12-05 12:44:42 -0300

    FINAL PUSH 2019

---
+++ TDA-GRAFO/Graph/__init__.py
@@ -0,0 +1,101 @@
+class Vertice():
+
+	def __init__(self, clave):
+		self.clave = clave
+		self.adyacentes = {}
+
+	def __str__(self):
+		return str(self.clave) + ' adyacente: ' + str([x.clave for x in self.adyacentes])
+
+	def agregarAdyacente(self, claveAdyacente, peso = 0):
+		self.adyacentes[claveAdyacente] = peso
+
+	def getAdyacentes(self):
+		return self.adyacentes.keys()
+
+	def getClave(self):
+		return self.clave
+
+	def getPeso(self, adyacente):
+		return self.adyacentes[adyacente]
+
+	def getMinPeso(self):
+
+		minimo = float('inf')
+		minAdyacente = None 
+
+		for adyacente in self.getAdyacentes():
+			if self.adyacentes[adyacente] < minimo:
+				minimo = self.adyacentes[adyacente]
+				minAdyacente = adyacente 
+
+		return minAdyacente 
+
+class Grafo():
+
+	def __init__(self, vertices = {}, cantVertices = 0):
+		self.vertices = vertices
+		self.cantVertices = cantVertices
+
+	def __iter__(self):
+		return iter(self.vertices.values())
+
+	def agregarVertice(self, nuevoVertice):
+		self.cantVertices += 1
+		verticeAux = Vertice(nuevoVertice)
+		self.vertices[nuevoVertice] = verticeAux
+		return verticeAux
+
+	def agregarArista(self, desde, hasta, peso = 0):
+
+		if desde not in self.vertices:
+			self.agregarVertice(desde)
+		if hasta not in self.vertices:
+			self.agregarVertice(hasta)
+
+		self.vertices[desde].agregarAdyacente(self.vertices[hasta], peso)
+		self.vertices[hasta].agregarAdyacente(self.vertices[desde], peso)
+
+	def getVertices(self):
+		return self.vertices.keys()
+
+
+	def BFS(self, inicio):
+
+		vertice = self.vertices[inicio]
+		queue = [vertice]
+		visitados = []
+
+		while queue:
+
+			nodo = queue.pop(0)
+
+			if nodo not in visitados:
+				visitados.append(nodo)
+
+				for adyacente in nodo.getAdyacentes():
+					queue.append(adyacente)
+
+		return visitados
+
+	def DFS(self, inicio):
+		visitados = []
+		aux = self.vertices[inicio]
+		self.DFSUtil(aux, visitados)
+		
+		return visitados
+
+	def DFSUtil(self, inicio, visitados):
+		if inicio not in visitados:
+			visitados.append(inicio)
+	
+		for adyacente in inicio.getAdyacentes():
+			if adyacente not in visitados:
+				visitados.append(adyacente)
+				self.DFSUtil(adyacente, visitados)
+
+
+
+
+
+   	
---
+++ TDA-GRAFO/test.py
@@ -0,0 +1,53 @@
+from Graph import *
+
+
+if __name__ == '__main__':
+
+    g = Grafo()
+
+    g.agregarVertice('a')
+    g.agregarVertice('b')
+    g.agregarVertice('c')
+    g.agregarVertice('d')
+    g.agregarVertice('e')
+    g.agregarVertice('f')
+
+    g.agregarArista('a', 'b', 7)  
+    g.agregarArista('a', 'c', 9)
+    g.agregarArista('a', 'f', 14)
+    g.agregarArista('b', 'c', 10)
+    g.agregarArista('b', 'd', 15)
+    g.agregarArista('c', 'd', 11)
+    g.agregarArista('c', 'f', 2)
+    g.agregarArista('d', 'e', 6)
+    g.agregarArista('e', 'f', 9)
+
+
+    for v in g:
+        print(v)
+        for w in v.getAdyacentes():
+            vid = v.getClave()
+            wid = w.getClave()
+            print ('( %s , %s, %3d)'  % ( vid, wid, v.getPeso(w)))
+
+
+    g.DFS('a')
+
+    
+    g2 = Grafo()
+    g2.agregarVertice('a')
+    g2.agregarVertice('b')					
+    g2.agregarVertice('c')							
+    g2.agregarVertice('d')
+    g2.agregarVertice('f')
+    g2.agregarVertice('e')
+
+    g2.agregarArista('a', 'b', 0)
+    g2.agregarArista('b', 'c', 0)
+    g2.agregarArista('c', 'f', 0)
+    g2.agregarArista('b', 'd', 0)
+    g2.agregarArista('d', 'e', 0)
+    g2.agregarArista('e', 'a', 0)
+    g2.DFS('a')
+
+    g2.BFS('a')
